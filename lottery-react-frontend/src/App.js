3e60a7c873ce5c563a94c8990d60ff4aca0c272f
bugattie <m.mohsinbarkaati@gmail.com>
2023-08-25 00:50:15 +0500

    react code

--- lottery-react-frontend/src/App.js
+++ lottery-react-frontend/src/App.js
@@ -1,29 +1,87 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
+import web3 from "./web3";
 import lottery from "./lottery";
 
 const App = () => {
   const [manager, setManager] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState("");
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState("");
 
-  const fetchManager = async () => {
-    try {
-      const address = await lottery.methods.manager().call();
-      // const balance = await web3.eth.getBalance(lottery.options.address);
-      console.log(address);
-    } catch (error) {
-      console.log(`Error fetching ${error}`);
-    }
+  const fetchData = async () => {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    setManager(manager);
+    setPlayers(players);
+    setBalance(balance);
   };
 
   useEffect(() => {
-    fetchManager();
+    fetchData();
   }, []);
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    setMessage("Waiting on transaction success...");
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, "ether"),
+    });
+
+    setMessage("You have been entered!");
+  };
+
+  const handleClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    setMessage("Waiting on transaction success...");
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+
+    setMessage("A winner has been picked!");
+  };
+
   return (
-    <div>
+    <>
       <h2>Lottery Contract</h2>
-      <p>Contract is managed by: {manager}</p>
-    </div>
+      <p>
+        This contract is managed by {manager}. There are currently{" "}
+        {players.length} people entered, competing to win{" "}
+        {web3.utils.fromWei(balance, "ether")} ether!
+      </p>
+
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
+
+      <hr />
+
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={handleClick}>Pick a winner!</button>
+
+      <hr />
+
+      <h1>{message}</h1>
+    </>
   );
 };
 
--- lottery-react-frontend/src/web3.js
+++ lottery-react-frontend/src/web3.js
@@ -1,8 +1,8 @@
 import Web3 from "web3";
 
-const web3 = new Web3(window.ethereum);
-
 // Since, window.ethereum.enable().then(() => {}); is deprecated and may removed in future so the below line will work anyway
 window.ethereum.request({ method: "eth_requestAccounts" });
+
+const web3 = new Web3(window.ethereum);
 
 export default web3;
